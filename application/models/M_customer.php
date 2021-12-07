9ce2c428f0740024e8449f7d04c854f9fdd51c88
AnnaKurniaty <Annakuykuy25@gmail.com>
2021-12-07 15:55:03 +0700

    delet

--- application/models/M_customer.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-class M_customer extends CI_Model
-{ 
-  public function login($table, $where){
-      return $this->db->get_where($table, $where);
-  }	
-}
