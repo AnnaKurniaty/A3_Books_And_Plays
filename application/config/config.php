ea34984c05b59c6c19d8c5d8c9f08b71e3826cf6
AnnaKurniaty <Annakuykuy25@gmail.com>
2021-12-07 18:00:01 +0700

    1

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Books_And_Plays';
+$config['base_url'] = 'http://localhost/A3_Books_And_Plays';
 
 /*
 |--------------------------------------------------------------------------
